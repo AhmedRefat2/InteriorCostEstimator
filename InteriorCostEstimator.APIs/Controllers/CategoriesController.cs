d72a579649e7be47d4734df7db6162fe7f70df28
Basel638 <basselsameh2004@gmail.com>
2026-04-25 19:00:12 +0300

    Commit

--- InteriorCostEstimator.APIs/Controllers/CategoriesController.cs
+++ InteriorCostEstimator.APIs/Controllers/CategoriesController.cs
@@ -1,7 +1,10 @@
 ﻿using InteriorCostEstimator.Application.Features.CategoryFeature.DTOs;
 using InteriorCostEstimator.Application.Features.CategoryFeature.Services;
+using InteriorCostEstimator.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace InteriorCostEstimator.APIs.Controllers
 {
@@ -10,17 +13,23 @@ namespace InteriorCostEstimator.APIs.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _service;
+        private readonly AppDbContext _context;
 
-        public CategoriesController(ICategoryService service)
+        public CategoriesController(ICategoryService service, AppDbContext context)
         {
             _service = service;
+            _context = context;
         }
 
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            return Ok(await _service.GetAllAsync());
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            var vendorId = _context.Vendors.Where(v => v.UserId == userId).Select(v => v.Id).FirstOrDefault();
+
+            return Ok(await _service.GetAllAsync(vendorId));
         }
 
 
